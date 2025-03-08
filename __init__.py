be770926558771740b510b147f63af5df63cae4f
Corentin Santos <corentin.santos@etu.unistra.fr>
2025-03-08 20:04:52 +0100

    Test with Worley noise for procedural creation of the board

--- __init__.py
+++ __init__.py
@@ -1,29 +1,37 @@
-import numpy as np
 import random
+import numpy as np
 import matplotlib.pyplot as plt
+from noise import snoise2
 from matplotlib.widgets import RadioButtons
 
+
 class Case:
-    '''
+    """
     Class to represent a case in the game board.
-    '''
-    def __init__(self, x, y):
+    """
+
+    def __init__(self, x, y, color="white"):
         self.x = x
         self.y = y
-        self.color = 'white'
+        self.color = color
         self.queen = False
         self.is_checked = False
 
     def __repr__(self):
-        return f'({self.x}, {self.y})'
+        return f"({self.x}, {self.y})"
+
 
 class GameBoard:
-    '''
+    """
     Class to represent the game board for the Queens game.
-    '''
+    """
+
     def __init__(self, size=7):
         self.size = size
         self.board = [[Case(x, y) for x in range(size)] for y in range(size)]
+
+    def set_case(self, x, y, case):
+        self.board[y][x] = case
 
     def get_colors(self):
         transformed_board_colors = np.zeros((self.size, self.size), dtype=object)
@@ -31,7 +39,7 @@ class GameBoard:
             for j in range(self.size):
                 transformed_board_colors[i, j] = self.board[i][j].color
         return transformed_board_colors
-    
+
     def get_queens(self):
         transformed_board_is_queen = np.zeros((self.size, self.size), dtype=bool)
         for i in range(self.size):
@@ -45,41 +53,57 @@ class GameBoard:
             for j in range(self.size):
                 transformed_board_is_checked[i, j] = self.board[i][j].is_checked
         return transformed_board_is_checked
-    
+
     def get_all(self):
         return self.get_colors(), self.get_queens(), self.get_checked()
 
+
 class GameBoardVisualizer:
-    '''
+    """
     Class to handle the visualization and interaction of the game board.
-    '''
+    """
+
     color_map = {
-        'Red': 'red',
-        'Green': 'green',
-        'Blue': 'blue',
-        'Orange': 'orange',
-        'Yellow': 'yellow',
-        'Cyan': 'cyan',
-        'Magenta': 'magenta',
-        'Purple': 'purple',
-        'Brown': 'brown',
-        'Pink': 'pink',
-        'Yellowgreen': 'yellowgreen',
-        'White': 'white'
+        "Red": "red",
+        "Green": "green",
+        "Blue": "blue",
+        "Orange": "orange",
+        "Yellow": "yellow",
+        "Cyan": "cyan",
+        "Magenta": "magenta",
+        "Purple": "purple",
+        "Brown": "brown",
+        "Pink": "pink",
+        "Yellowgreen": "yellowgreen",
+        "White": "white",
     }
 
     def __init__(self, game_board):
         self.game_board = game_board
         self.size = game_board.size
         self.fig, self.ax = plt.subplots(figsize=(12, 12))
-        self.ax.set_xticks(np.arange(-.5, self.size, 1))
-        self.ax.set_yticks(np.arange(-.5, self.size, 1))
+        self.ax.set_xticks(np.arange(-0.5, self.size, 1))
+        self.ax.set_yticks(np.arange(-0.5, self.size, 1))
         self.ax.set_xticklabels([])
         self.ax.set_yticklabels([])
-        self.ax.grid(color='black', linestyle='-', linewidth=2)
-        self.rects = [[self.ax.add_patch(plt.Rectangle((j-0.5, i-0.5), 1, 1, edgecolor='black', facecolor=self.game_board.board[j][i].color)) for j in range(self.size)] for i in range(self.size)]
-        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
-        self.selected_color = 'red'
+        self.ax.grid(color="black", linestyle="-", linewidth=2)
+        self.rects = [
+            [
+                self.ax.add_patch(
+                    plt.Rectangle(
+                        (j - 0.5, i - 0.5),
+                        1,
+                        1,
+                        edgecolor="black",
+                        facecolor=self.game_board.board[j][i].color,
+                    )
+                )
+                for j in range(self.size)
+            ]
+            for i in range(self.size)
+        ]
+        self.fig.canvas.mpl_connect("button_press_event", self.on_click)
+        self.selected_color = "red"
         self.add_color_buttons()
 
     def on_click(self, event):
@@ -102,10 +126,12 @@ class GameBoardVisualizer:
     def show(self):
         plt.show()
 
+
 class Queen:
-    '''
+    """
     Class to represent a queen in the game board.
-    '''
+    """
+
     def __init__(self, game_board, x, y):
         self.game_board = game_board
         case = self.game_board.board[y][x]
@@ -115,7 +141,7 @@ class Queen:
             self.y = y
             self.color = case.color
             case.queen = True
-            
+
             # Check the row, column and the squares diagonally
             if 0 <= x - 1 < self.game_board.size and 0 <= y - 1 < self.game_board.size:
                 self.game_board.board[y - 1][x - 1].is_checked = True
@@ -134,60 +160,63 @@ class Queen:
                     self.game_board.board[y][x + i].is_checked = True
                 if 0 <= x - i < self.game_board.size:
                     self.game_board.board[y][x - i].is_checked = True
-            
+
             # Also check all the cases with the same color as the queen
             for i in range(self.game_board.size):
                 for j in range(self.game_board.size):
                     if self.game_board.board[j][i].color == self.color:
                         self.game_board.board[j][i].is_checked = True
-        
+
         else:
-            print('The case is already checked by another queen')
+            print("The case is already checked by another queen")
 
     def __repr__(self):
-        return f'Queen({self.x}, {self.y}, {self.color})'
+        return f"Queen({self.x}, {self.y}, {self.color})"
 
-def is_legal(board, x, y):
-    case = board.board[y][x]
-    if case.color == 'white':
-        return False
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    return any(
-        0 <= x + dx < board.size and 0 <= y + dy < board.size and board.board[y + dy][x + dx].color == case.color
-        for dx, dy in directions
-    )
 
-def create_board(size=7):
-    board = GameBoard(size)
-    colors = ['red', 'green', 'blue', 'orange', 'yellow', 'cyan', 'magenta', 'purple', 'brown', 'pink', 'yellowgreen']
-    random.shuffle(colors)
-    colors_rd = colors[:size]
-    
-    # First, assign random colors to the board
-    for row in board.board:
-        for case in row:
-            case.color = random.choice(colors_rd)
-    
-    # Then, ensure that at least two adjacent cases have the same color
-    for row in board.board:
-        for case in row:
-            while not is_legal(board, case.x, case.y):
-                case.color = random.choice(colors_rd)
-    
-    # Ensure the board has exactly 'size' number of colors
-    used_colors = set()
-    for row in board.board:
-        for case in row:
-            used_colors.add(case.color)
-    if len(used_colors) != size:
-        return create_board(size)
-    
+def worley_noise(width, height, scale):
+    noise = np.zeros((width, height))
+    for x in range(width):
+        for y in range(height):
+            noise[x][y] = snoise2(x / scale, y / scale, octaves=1)
+    return noise
+
+
+def generate_board(noise, num_regions):
+    # Normalize noise to range [0, 1]
+    noise = (noise - noise.min()) / (noise.max() - noise.min())
+
+    # Create discrete regions
+    regions = np.floor(noise * num_regions).astype(int)
+
+    # Generate a unique color for each region
+    colors = plt.cm.get_cmap("tab20", num_regions)
+    board = colors(regions)
+
     return board
 
-if __name__ == '__main__':
+
+def create_board(size=7):
+    scale = 10.0  # Adjust the scale for a smaller board
+    num_regions = size  # Number of unique color zones
+
+    noise = worley_noise(size, size, scale)
+    board_colors = generate_board(noise, num_regions)
+
+    board = GameBoard(size)
+    for x in range(size):
+        for y in range(size):
+            color = board_colors[x, y]
+            board.set_case(x, y, Case(x, y, color=color))
+
+    return board
+
+
+if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser('Queens Game')
-    parser.add_argument('-size', type=int, default=7, help='Size of the game board')
+
+    parser = argparse.ArgumentParser("Queens Game")
+    parser.add_argument("-size", type=int, default=7, help="Size of the game board")
     args = parser.parse_args()
 
     board = create_board(args.size)
